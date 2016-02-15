4bedd39b2c731119a81810037e05b881a7cf427f
Orion Despo <oriondespo@DN0a227866.SUNet>
2016-02-15 09:30:30 -0800

    hey

--- archive_app/settings.py
+++ archive_app/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 import dj_database_url
 
+# Configure db
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES = { 'default' : db_from_env}
 ######################################################### SIMILARITY OPTIMIZATION PARAMETERS
